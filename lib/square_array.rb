90b66dffae55359a8ef81f697ba236a9cf32cf77
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-01 11:08:53 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
   # your code here
+  # for every element in the array, take that number of that element and square and print out the new array
+
+  i = 0
+  new_array = []
+
+  while i < array.length do
+    new_array >> (array[i]**2)
+    i += 1
+  end
+
 end
